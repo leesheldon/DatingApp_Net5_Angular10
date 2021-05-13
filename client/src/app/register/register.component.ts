5a29695af2cc8d15c39f80be25f04d3843a2894e
Sheldon <lhanco@gmail.com>
2021-05-13 13:06:50 +0700

    06-Client- route guard- toast notifications - bootswatch theme

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -10,7 +11,8 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor(private accountService: AccountService) { }
+  constructor(private accountService: AccountService,
+    private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -20,9 +22,8 @@ export class RegisterComponent implements OnInit {
       console.log(response);
     }, error => {
       console.log(error);
-    });
-
-    this.cancel();
+      this.toastr.error(error.error);
+    });    
   }
 
   cancel() {
