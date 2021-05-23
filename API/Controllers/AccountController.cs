c98c20f88f91940b8b1005db8756985bdc623070
Sheldon <lhanco@gmail.com>
2021-05-23 23:50:46 +0700

    12-Client side validation, reuse text/date input

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -6,6 +6,7 @@ using API.Data;
 using API.DTOs;
 using API.Entities;
 using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,39 +16,41 @@ namespace API.Controllers
     {
         private readonly DataContext _context;
         private readonly ITokenService _tokenService;
+        private readonly IMapper _mapper;
 
-        public AccountController(DataContext context, ITokenService tokenService)
+        public AccountController(DataContext context, ITokenService tokenService, IMapper mapper)
         {
+            _mapper = mapper;
             _tokenService = tokenService;
             _context = context;
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<RegisterUserDto>> Register(RegisterDto registerDto)
+        public async Task<ActionResult<ReturnLoginUserDto>> Register(RegisterDto registerDto)
         {
             if (await UserExists(registerDto.Username)) return BadRequest("This user name is taken!");
 
+            var user = _mapper.Map<AppUser>(registerDto);
+
             using var hmac = new HMACSHA512();
 
-            var user = new AppUser
-            {
-                UserName = registerDto.Username.ToLower(),
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
-                PasswordSalt = hmac.Key
-            };
+            user.UserName = registerDto.Username.ToLower();
+            user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
+            user.PasswordSalt = hmac.Key;
 
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
 
-            return new RegisterUserDto
+            return new ReturnLoginUserDto
             {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                KnownAs = user.KnownAs
             };
         }
 
         [HttpPost("login")]
-        public async Task<ActionResult<RegisterUserDto>> Login(LoginDto loginDto)
+        public async Task<ActionResult<ReturnLoginUserDto>> Login(LoginDto loginDto)
         {
             var user = await _context.Users
                 .Include(p => p.Photos)
@@ -64,11 +67,12 @@ namespace API.Controllers
                 if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("This password is invalid!");
             }
 
-            return new RegisterUserDto
+            return new ReturnLoginUserDto
             {
                 Username = user.UserName,
                 Token = _tokenService.CreateToken(user),
-                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
+                KnownAs = user.KnownAs
             };
         }
 
--- API/DTOs/ReturnLoginUserDto.cs
+++ API/DTOs/ReturnLoginUserDto.cs
@@ -1,11 +1,14 @@
 namespace API.DTOs
 {
-    public class RegisterUserDto
+    public class ReturnLoginUserDto
     {
         public string Username { get; set; }
         
         public string Token { get; set; }
 
         public string PhotoUrl { get; set; }
+
+        public string KnownAs { get; set; }
+
     }
 }
